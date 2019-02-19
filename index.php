6c1fb409d89cb09be25a5d61f9c5091dd2771d4f
mustelidae1 <othm@live.com>
2019-02-18 20:40:20 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<html> 
+<head>
+   <link rel="stylesheet" href="style.css"> 
+   <link href="https://fonts.googleapis.com/css?family=Cinzel|Arapey" rel="stylesheet">
+</head> 
+<body> 
+<div id="main"> 
+<div id="navbar">
+  <div id="logo"><img src="logo-small.png"></div>  
+  <div id="navButtons">
+    <ul> 
+    <li> <p>Log In</p> </li> 
+    <li> <p>Sign Up</p> </li> 
+    </ul>
+   </div>  
+</div> 
+<div id="top"> 
+</div> 
+
+<div id="mid"> 
+<div id="midContent">
+   <form method="post">
+      <h1>Genre</h1>
+      <div id="radioBSection"> 
+      <div class="radioB"><input type="radio" name="genre" value="comedy"> <p>Comedy</p> </div> 
+      <div class="radioB"><input type="radio" name="genre" value="action"> <p>Action</p> </div> 
+      <div class="radioB"><input type="radio" name="genre" value="general"> <p>General</p> </div>  
+      </div> 
+      <h1>Characters</h1>
+      <div><input class="textBox" type="text" name="char1"><input type="radio" name="mainChar" value="1"></div> 
+      <div><input class="textBox" type="text" name="char2"><input type="radio" name="mainChar" value="2"> </div> 
+      <div><input class="textBox" type="text" name="char3"><input type="radio" name="mainChar" value="3"></div> 
+      <input type="submit" value="Submit">   
+   </form>  
+</div> 
+</div> 
+<div id="bottom"> </div>
+<div id="footer">&copy; 2019 Olivia Thomas. Images used with permission from writersplanet.org and kisspng.</div>  
+</div> 
+</body> 
+</html> 
